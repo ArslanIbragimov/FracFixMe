df5d792b4421ec7a312af2e6904f69616dcd91fc
ArslanIbragimov <arslanibragimov366@gmail.com>
2023-10-12 20:48:58 +0500

    Реализация обработчика формы регистрации

---
+++ src/main/java/letscodeguide/FracFixMe/controller/RegisterController.java
@@ -0,0 +1,39 @@
+package letscodeguide.FracFixMe.controller;
+
+
+import ch.qos.logback.core.net.SMTPAppenderBase;
+import letscodeguide.FracFixMe.other.SGetter.RegisterSGetter;
+import letscodeguide.FracFixMe.other.model.JPARespositories.UsersRepository;
+import letscodeguide.FracFixMe.other.model.tables.UsersTable;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class RegisterController {
+    final public static String handle = "register";
+    @Autowired
+    private final UsersRepository usersRepository;
+
+    public RegisterController(UsersRepository usersRepository) {
+        this.usersRepository = usersRepository;
+    }
+
+    @GetMapping(handle)
+    public String onFillRegisterForm(Model model){
+        model.addAttribute("register", new RegisterSGetter());
+        return "register";
+    }
+    @PostMapping(handle)
+    public String onClickSubmitRegisterForm(@ModelAttribute RegisterSGetter registerSGetter,  Model model){
+        model.addAttribute("register", registerSGetter);
+        //TODO: Исправить ошибку при отправке формы
+        UsersTable appendTable = new UsersTable(registerSGetter.getUsername(),registerSGetter.getPassword(), registerSGetter.getEmail());
+        //TODO: Проблема из-за : , но без него не сохранятся данные в таблице
+        //usersRepository.save(appendTable);
+        return "successfulLogin";
+    }
+}
