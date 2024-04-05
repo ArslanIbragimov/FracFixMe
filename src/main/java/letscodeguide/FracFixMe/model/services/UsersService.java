31e5e023fcedecca9dbd635301f3d3992aebe95f
ArslanIbragimov <darovinavgust@gmail.com>
2024-04-05 18:56:19 +0500

    Some optimizations

--- src/main/java/letscodeguide/FracFixMe/model/services/UsersService.java
+++ src/main/java/letscodeguide/FracFixMe/model/services/UsersService.java
@@ -1,11 +1,13 @@
 package letscodeguide.FracFixMe.model.services;
 
 
+import jakarta.servlet.http.HttpServletRequest;
 import letscodeguide.FracFixMe.controller.AuthenticationController;
 import letscodeguide.FracFixMe.model.JPARespositories.UsersRepository;
 import letscodeguide.FracFixMe.model.tables.UsersTable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class UsersService {
@@ -13,18 +15,17 @@ public class UsersService {
     private final UsersRepository usersRepository;
 
     public UsersService(UsersRepository usersRepository) {
-
         this.usersRepository = usersRepository;
     }
 
-
-
+    @Transactional
     public String validateUsersForm(String username, String password, String returnablePage) {
+
         UsersTable data = usersRepository.findByUsernameAndPassword(username, password);
         if (data != null) {
             return returnablePage;
         } else {
-            return AuthenticationController.handle;
+            return AuthenticationController.getHandle();
         }
     }
 
