4e75bd0e3fbfe9efa77c4a3612beb9d612cafebe
ArslanIbragimov <darovinavgust@gmail.com>
2024-05-22 13:40:59 +0500

    - Small code improvements - Added try-catch - Extended opportunity for the testing

--- src/main/java/letscodeguide/FracFixMe/controller/RegisterController.java
+++ src/main/java/letscodeguide/FracFixMe/controller/RegisterController.java
@@ -1,30 +1,29 @@
 package letscodeguide.FracFixMe.controller;
 
 
+import letscodeguide.FracFixMe.model.services.UsersService;
 import letscodeguide.FracFixMe.other.SGetter.RegisterSGetter;
-import letscodeguide.FracFixMe.model.JPARespositories.UsersRepository;
+import letscodeguide.FracFixMe.model.JPARepositories.UsersRepository;
 import letscodeguide.FracFixMe.model.tables.UsersTable;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 
 public class RegisterController {
     final private static String handle = "register";
     @Autowired
-    private final UsersRepository usersRepository;
+    private UsersRepository usersRepository;
 
-
-    public RegisterController(UsersRepository usersRepository) {
-        this.usersRepository = usersRepository;
-    }
+    @Autowired
+    private UsersService usersService;
 
     @GetMapping(handle)
-    public String onFillRegisterForm(Model model){
+    public String onFillingRegisterForm(Model model){
         model.addAttribute("register", new RegisterSGetter());
         return "register";
     }
@@ -33,7 +32,12 @@ public class RegisterController {
     public String onClickSubmitRegisterForm(@ModelAttribute RegisterSGetter registerSGetter,  Model model){
         model.addAttribute("register", registerSGetter);
         UsersTable appendTable = new UsersTable(registerSGetter.getUsername(),registerSGetter.getPassword(), registerSGetter.getEmail());
-        usersRepository.save(appendTable);
+        try {
+            usersService.registerUser(appendTable);
+        } catch (Exception ex){
+            model.addAttribute("error", ex.getMessage());
+            return "register";
+        }
         return "successfulLogin";
     }
 }
