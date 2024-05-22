ea3e4229afeaeeef1e54046565e214026dda48d4
ArslanIbragimov <darovinavgust@gmail.com>
2024-05-22 13:32:41 +0500

    Small code improvement

--- src/main/java/letscodeguide/FracFixMe/model/services/UsersService.java
+++ src/main/java/letscodeguide/FracFixMe/model/services/UsersService.java
@@ -1,39 +1,36 @@
 package letscodeguide.FracFixMe.model.services;
 
 
-import jakarta.servlet.http.HttpServletRequest;
 import letscodeguide.FracFixMe.controller.AuthenticationController;
-import letscodeguide.FracFixMe.model.JPARespositories.UsersRepository;
+import letscodeguide.FracFixMe.exceptions.DatabaseDisconnectException;
+import letscodeguide.FracFixMe.model.JPARepositories.UsersRepository;
 import letscodeguide.FracFixMe.model.tables.UsersTable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
 @Service
 public class UsersService {
     @Autowired
-    private final UsersRepository usersRepository;
-
-    public UsersService(UsersRepository usersRepository) {
-        this.usersRepository = usersRepository;
-    }
+    private UsersRepository usersRepository;
 
     @Transactional
-    public String validateUsersForm(String username, String password, String returnablePage) {
+    public String validateUsersForm(String username, String password, String returnablePage)  {
 
-        UsersTable data = usersRepository.findByUsernameAndPassword(username, password);
-        if (data != null) {
+        Optional<UsersTable> data = usersRepository.findByUsernameAndPassword(username, password);
+
+        if (data.isPresent()) {
             return returnablePage;
         } else {
             return AuthenticationController.getHandle();
         }
     }
-
-    //TODO Сделать форму регистрации:
-    //Создаем метод регистрации
-    //получаем данные из формы
-    //Создаём "ОБЪЕКТ" usersTable, сохраняем данные в сеттеры
-    //usersRepository.save("ОБЪЕКТ")
+    public UsersTable registerUser(UsersTable user) {
+        usersRepository.save(user);
+        return user;
+    }
 
 
 
