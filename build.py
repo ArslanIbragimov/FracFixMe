e2fdbbd9d465007fb89f5ac9df4fd35af0e100bd
ArslanIbragimov <darovinavgust@gmail.com>
2024-05-22 18:10:15 +0500

    Added feature which need to build project without tests

--- build.py
+++ build.py
@@ -3,12 +3,16 @@ import shutil
 import os
 from xml.etree import ElementTree
 
+withoutTests = True
+
 with open("./pom.xml") as f:
     tree = ElementTree.parse(f)
     version = tree.getroot().find("{http://maven.apache.org/POM/4.0.0}version").text
 mvn_path = shutil.which("mvn")
-subprocess.check_call([mvn_path, "package"])
-
+if withoutTests:
+    subprocess.check_call([mvn_path, "package", "-DskipTests"])
+else:
+    subprocess.check_call([mvn_path, "package"])
 os.chdir(os.path.join("target"))
 
 shutil.copy(f"./FracFixMe-{version}.jar", os.path.join("..", "site_docker", f"FracFixMe-{version}.jar"))
